d05e20bd205c4b1928215b35c96c5807dd9630cb
afrid-zaman <afridzaman57@gmail.com>
2025-08-04 21:12:10 -0400

    homework completed.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,31 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody2D;
 
+    private float _xSpeed;
+    private float _ySpeed;
+
+    public float speed = 3;
+
+    private string xInput = "Horizontal";
+    private string yInput = "Vertical";
+
+
+
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _xSpeed = Input.GetAxis(xInput);
+        _ySpeed = Input.GetAxis(yInput);
+
+        _rigidbody2D.velocity = new Vector2(_xSpeed, _ySpeed) * speed ;
+    }
 }
--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -2,5 +2,57 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    public GameObject prefab;
+    public GameObject prefab2;
+    public Transform bulletTrash;
+    public Transform bulletSpawn;
 
+    private const float Timer = 0.5f;
+    private float _currentTime = 0.5f;
+    private bool _canShoot = true;
+
+    private void Update()
+    {
+        timeMethod();
+
+        shoot();
+
+    }
+
+    private void shoot()
+    { 
+    if (Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
+        {
+            GameObject bullet = Instantiate(prefab, bulletSpawn.position, Quaternion.identity);
+
+             bullet.transform.SetParent(bulletTrash);
+
+            _canShoot = false;
+        }
+
+    if (Input.GetKeyDown(KeyCode.Mouse1)  && _canShoot)
+        {
+            GameObject bullet = Instantiate(prefab2, bulletSpawn.position, Quaternion.identity);
+
+            bullet.transform.SetParent(bulletTrash);
+
+            _canShoot = false;
+
+        }
+     }
+
+    private void timeMethod()
+    {
+        if (!_canShoot)
+        {
+            _currentTime -= Time.deltaTime;
+
+            if (_currentTime < 0)
+            {
+                _canShoot = true;
+                _currentTime = Timer;
+            }
+        }
+    }
+    
 }
